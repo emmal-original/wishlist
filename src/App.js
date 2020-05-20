ad0d365ba3beda996a025ef53d94ccdc558a2314
emmal-original <the.original.e@gmail.com>
2020-05-20 15:29:55 -0500

    User add wishes, mark wishes as granted, hide or display granted wishes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { WishlistBanner } from './WishlistBanner';
+import { WishCreator } from './WishCreator';
+import { WishRow } from './WishRow';
+import { VisibilityControl } from './VisibilityControl';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userName: "Emma",
+      wishes: [
+      ],
+      showGranted: true
+    }
+  }
+
+
+  createNewWish = (newWish) => {
+    if (!this.state.wishes.find(x => x.wish === newWish)) {
+      this.setState({
+        wishes: [
+          ...this.state.wishes,
+          {
+            wish: newWish,
+            granted: false
+          }
+        ]
+      },
+      () => localStorage.setItem("savedWishes", JSON.stringify(this.state))
+      )
+    }
+  }
+
+  wishTableRows = (grantedValue) => this.state.wishes.filter(item => item.granted === grantedValue).map(item =>
+    <WishRow key={item.wish} item={item} callback={this.toggleWish} />
   );
-}
 
-export default App;
+  toggleWish = (wishToggle) => this.setState(
+    {
+      wishes: this.state.wishes.map(item => item.wish === wishToggle.action ? { ...item, granted: !item.granted } : item)
+    },
+    () => localStorage.setItem("savedWishes", JSON.stringify(this.state))
+  );
+
+  componentDidMount = () => {
+    let data = localStorage.getItem("savedWishes");
+    this.setState(
+      data !=null ? JSON.parse(data) :
+      {
+        userName: "Emma",
+        wishes: [
+          {
+            wish: "Vaccine for COVID-19",
+            granted: false,
+          },
+          {
+            wish: "End to stay-at-home order",
+            granted: false,
+          },
+          {
+            wish: "Return to classroom with Centriq classmates",
+            granted: false,
+          },
+          {
+            wish: "Pursue a career that challenges and excites me",
+            granted: true
+          }
+        ],
+        showGranted: true
+      }
+    );
+  }
+
+
+  render() {
+    return (
+      <div>
+        <WishlistBanner name={this.state.userName} outstandingWishes={this.state.wishes} />
+
+        <div className="container-fluid">
+
+          <WishCreator callback={this.createNewWish} />
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Wish</th>
+                <th>Granted</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.wishTableRows(false)}
+            </tbody>
+          </table>
+          <div className="bg-info text-white text-center p-2">
+            <VisibilityControl description="Granted Wishes" isChecked={this.state.showGranted}
+              callback={(checked) => this.setState({ showGranted: checked })} />
+          </div>
+          {
+            this.state.showGranted &&
+            <table className="table table-striped table-bordered">
+              <thead>
+                <tr>
+                  <th>Wish</th>
+                  <th>Granted</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.wishTableRows(true)}
+              </tbody>
+            </table>
+          }
+        </div>
+      </div>
+    );
+  }
+};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';//reference bootstrap library we pulled in with NPM
 
 ReactDOM.render(
   <React.StrictMode>
