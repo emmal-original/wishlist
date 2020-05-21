2c1fe16a52c748eed7c70a6c85d2f7db1cc68dfa
emmal-original <the.original.e@gmail.com>
2020-05-21 13:40:57 -0500

    Fix bug on Granted checkbox

--- src/WishlistBanner.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-
-export class WishlistBanner extends Component {
-    render = () =>
-    <h4 className="bg-info text-white text-center p-2">
-        {this.props.name}'s Wishlist 
-        ({this.props.outstandingWishes.filter(w => !w.granted).length} wishes yet to be granted)
-    </h4>
-}
